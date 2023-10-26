2eb8fca60491e29c321de5f13442ffc5a4e27069
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2023-10-26 21:40:42 +0300

    task1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+let interval;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick() {
+  refs.startBtn.setAttribute('disabled', 'true');
+  refs.stopBtn.removeAttribute('disabled');
+  interval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  refs.stopBtn.addEventListener('click', onStopBtnClick);
+}
+
+function onStopBtnClick() {
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', 'true');
+  clearInterval(interval);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
